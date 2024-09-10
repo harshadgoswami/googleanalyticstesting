a3248a04c962f736cd254c3c8845db1ed1d0f6dd
Harshad Goswami <harshad.goswami@gmail.com>
2024-09-10 11:06:18 +0530

    google analytics

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { sendToGoogleAnalytics4 } = require("./lib/sendToGoogleAnalyticst");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+//console.log(process.env);
 
 app.set("view engine", "ejs");
 
@@ -11,15 +13,16 @@ app.get("/", (req, res) => {
 
 app.get("/mock/deposit/", async (req, res) => {
   await sendToGoogleAnalytics4(
-    process.env.NEXT_PUBLIC_ANALYTICS_CODE, // Your GA4 Measurement ID
+    process.env.MEASUREMENT_ID, // Your GA4 Measurement ID
     process.env.ANALYTICS_API_SECRET, // Your GA4 API Secret
     "deposit", // Event name
     {
       value: 10,
       currency: "USD",
-      transaction_id: "02D3xjsdf234",
-      from: "324sdfsdf",
-      to: "02D3xjsdf234",
+      transaction_id:
+        "0x7abe2c1bd21f4282cf2492575c5af53756576e27c67c9be7bb4201bc9154fe49",
+      from: "0x7abe2c1bd21f4282cf2492575c5af53756576e27c67c9be7bb4201bc9154fe49",
+      to: "0x7abe2c1bd21f4282cf2492575c5af53756576e27c67c9be7bb4201bc9154fe49",
     }
   );
   res.status(201).json({ message: "Deposit Mock Run Successfully." });
